063d78a6ebabc2c86ec3e6c3fac7cbe31e691e25
Tomás Pettit <G00419414@atu.ie>
2026-02-05 21:22:27 +0000

    Updating package.json('s) and vite.config.ts.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,39 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Guardians of the Chess Grandmaster',
+        short_name: 'GOTCG',
+        description: 'A chess game with a twist, combining strategy and adventure.',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable'
+          }
+        ]
+      }
+    })
+  ],
   server: {
     proxy: {
       // Proxy API routes to backend
