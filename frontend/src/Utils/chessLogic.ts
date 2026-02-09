753638b00ba1d8eba4aeda6a9d83e9211dc06806
Tomás Pettit <G00419414@atu.ie>
2026-02-09 13:43:04 +0000

    Update another problem for castling the king and the rook or something else.

--- frontend/src/Utils/chessLogic.ts
+++ frontend/src/Utils/chessLogic.ts
@@ -4,29 +4,29 @@ export function createInitialBoard(): Board {
   const board: Board = Array(8).fill(null).map(() => Array(8).fill(null));
   
   // Black pieces
-  board[0][0] = { type: 'rook', color: 'black' };
-  board[0][1] = { type: 'knight', color: 'black' };
-  board[0][2] = { type: 'bishop', color: 'black' };
-  board[0][3] = { type: 'queen', color: 'black' };
-  board[0][4] = { type: 'king', color: 'black' };
-  board[0][5] = { type: 'bishop', color: 'black' };
-  board[0][6] = { type: 'knight', color: 'black' };
-  board[0][7] = { type: 'rook', color: 'black' };
+  board[0][0] = { type: 'rook', color: 'black', hasMoved: false };
+  board[0][1] = { type: 'knight', color: 'black', hasMoved: false };
+  board[0][2] = { type: 'bishop', color: 'black', hasMoved: false };
+  board[0][3] = { type: 'queen', color: 'black', hasMoved: false };
+  board[0][4] = { type: 'king', color: 'black', hasMoved: false };
+  board[0][5] = { type: 'bishop', color: 'black', hasMoved: false };
+  board[0][6] = { type: 'knight', color: 'black', hasMoved: false };
+  board[0][7] = { type: 'rook', color: 'black', hasMoved: false };
   for (let col = 0; col < 8; col++) {
-    board[1][col] = { type: 'pawn', color: 'black' };
+    board[1][col] = { type: 'pawn', color: 'black', hasMoved: false };
   }
   
   // White pieces
-  board[7][0] = { type: 'rook', color: 'white' };
-  board[7][1] = { type: 'knight', color: 'white' };
-  board[7][2] = { type: 'bishop', color: 'white' };
-  board[7][3] = { type: 'queen', color: 'white' };
-  board[7][4] = { type: 'king', color: 'white' };
-  board[7][5] = { type: 'bishop', color: 'white' };
-  board[7][6] = { type: 'knight', color: 'white' };
-  board[7][7] = { type: 'rook', color: 'white' };
+  board[7][0] = { type: 'rook', color: 'white', hasMoved: false };
+  board[7][1] = { type: 'knight', color: 'white', hasMoved: false };
+  board[7][2] = { type: 'bishop', color: 'white', hasMoved: false };
+  board[7][3] = { type: 'queen', color: 'white', hasMoved: false };
+  board[7][4] = { type: 'king', color: 'white', hasMoved: false };
+  board[7][5] = { type: 'bishop', color: 'white', hasMoved: false };
+  board[7][6] = { type: 'knight', color: 'white', hasMoved: false };
+  board[7][7] = { type: 'rook', color: 'white', hasMoved: false };
   for (let col = 0; col < 8; col++) {
-    board[6][col] = { type: 'pawn', color: 'white' };
+    board[6][col] = { type: 'pawn', color: 'white', hasMoved: false };
   }
   
   return board;
