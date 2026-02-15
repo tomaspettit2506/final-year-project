86c2c1fa5b75c7def53e634266d620fa6036ac3b
Tomás Pettit <G00419414@atu.ie>
2026-02-15 19:30:12 +0000

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/final-year-project/sw.js', { scope: '/final-year-project/' })})}
